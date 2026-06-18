0b6c8108d455036f775e470fb462592689d08345
Rashmiranjan07 <heyyrashmiranjan02@gmial.com>
2026-06-18 21:29:46 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <h1>code step by step</h1>
+//       <h1>hello React 19</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+function App() {
+  return (
+    <div>
+      <h1>First Component</h1>
+      <Fruit />
+      <Colours />
+    </div>
+  );
+}
+
+function Fruit() {
+  return (
+    <h1>Apple</h1>
+  );
+}
+
+function Colours() {
+  return (
+    <h1>Red</h1>
+  );
+}
+
+export default App;
+
+ 
