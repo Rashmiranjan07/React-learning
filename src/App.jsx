a86c7c83e9e39fa0472a14e5cbb3228a2bb69cf1
Rashmiranjan07 <heyyrashmiranjan02@gmial.com>
2026-06-19 20:23:50 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,19 @@
 
 function App() {
   const name="rashmiranjan"
+  let x=10;
+  let y=20;
+
+  function fruit(){
+    return "Apple"
+  }
   return (
     <div>
   
 <h1>JSX with Curly Braces</h1>
 <h1>{name}</h1>
-
-
+<h1>{x+y}</h1>
+<h1>{fruit()}</h1>
     </div>
   );
 }
