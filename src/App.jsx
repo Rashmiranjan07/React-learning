9ac97927d264ed93335ccee079b3627e06f6609c
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-22 19:48:51 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -109,11 +109,16 @@
 import { useState } from "react";
 
 function App() {
+  const [fruit, setFruit] = useState("Apple");
+  const handleFruit = () => {
+    setFruit("Banana")
+  }
 
   return (
     <div>
       <h1>State in React JS</h1>
-
+      <h1>{fruit}</h1>
+      <button onClick={handleFruit}>Change Fruit Name</button>
     </div>
   );
 }
