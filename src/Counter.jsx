5adaecb4392136778ecdcd190642265d2ec4a876
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-22 20:01:50 +0530

    added

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-const Counter=()=>{
-    const [count,setCount]=useState(0)
-    return(
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const [rCount, setrCount] = useState(10)
+    return (
         <div>
             <h1>Counter:{count}</h1>
-            <button onClick={()=>setCount(count+1)}> Update Counter </button>
+            <h2>Reverse Counter:{rCount}</h2>
+            <button onClick={() => setCount(count + 1)}> Update Counter </button>
+            <button onClick={() => setrCount(rCount - 1)}> Reverse Counter </button>
+
         </div>
     )
 }
