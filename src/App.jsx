9ad95eca0c5a94085b4205545a890b67834eca01
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-22 19:25:34 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -87,20 +87,32 @@
 
 // Event & Function call 
 
-function App(){
-  function callFun(){
-    alert("function called")
-  }
+// function App(){
+//   function callFun(){
+//     alert("function called")
+//   }
 
-const fruit=(name)=>{
-  alert(name)
-}
-   
+// const fruit=(name)=>{
+//   alert(name)
+// }
+
+//   return (
+//     <div>
+//       <h1>Event & Function call </h1>
+//       <button onClick={()=>fruit("apple")}>Apple</button>
+//       <button onClick={()=>fruit("banana")}>Banana</button>
+//     </div>
+//   );
+// }
+
+
+
+function App() {
+
+
   return (
     <div>
-      <h1>Event & Function call </h1>
-      <button onClick={()=>fruit("apple")}>Apple</button>
-      <button onClick={()=>fruit("banana")}>Banana</button>
+
     </div>
   );
 }
