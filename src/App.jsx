f90b8b3dadde378a65d5b639b6c0d18c15a26fac
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-22 20:58:19 +0530

    addeds

--- src/App.jsx
+++ src/App.jsx
@@ -155,28 +155,32 @@
 
 /* Multiple Condition in React Toggle  */
 
-import { useState } from "react"
+// import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <div>
-      <h1>Multiple Condition in React Toggle </h1>
-      <h1>{count}</h1>
-      <button onClick={()=>setCount(count+1)}>Counter</button>
-      {
-        count == 0 ? <h1>Condition 0 </h1>
-         : count==1? <h1>Condition 1 </h1>
-         : count==2? <h1>Condition 2 </h1>
-         : count==3? <h1>Condition 3 </h1>
-         : count==4? <h1>Condition 4 </h1>
-         : count==5? <h1>Condition 5 </h1>
-         : <h1>Other condition</h1>
+// function App() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <h1>Multiple Condition in React Toggle </h1>
+//       <h1>{count}</h1>
+//       <button onClick={()=>setCount(count+1)}>Counter</button>
+//       {
+//         count == 0 ? <h1>Condition 0 </h1>
+//          : count==1? <h1>Condition 1 </h1>
+//          : count==2? <h1>Condition 2 </h1>
+//          : count==3? <h1>Condition 3 </h1>
+//          : count==4? <h1>Condition 4 </h1>
+//          : count==5? <h1>Condition 5 </h1>
+//          : <h1>Other condition</h1>
 
-      }
-    </div>
-  )
-}
+//       }
+//     </div>
+//   )
+// }
+
+
+/* Props in React Js */
+
 
 export default App;
 
