5c087acb99fc25d65bfb9bba5db4198fd9f53819
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-23 13:11:34 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -275,30 +275,30 @@
 /* Controlled Components */
 
 import { useState } from "react";
-function App(){
-const [name,setName]=useState('r');
-const [password,setPassword]=useState('');
-const [email,setEmail]=useState('');
+function App() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   return (
     <div>
-<h1>Controller Component</h1>
-<form action="" method="get">
+      <h1>Controller Component</h1>
+      <form action="" method="get">
 
-  <input type="text" placeholder="Enter name " />
-  <br /> <br />
-<input type="password" placeholder="Enter password " />
-<br /><br />
-<input type="email" placeholder="Enter email " />
-<br /><br />
+        <input type="text" onChange={(event) => setName(event.target.value)} placeholder="Enter name " />
+        <br /> <br />
+        <input type="password" onChange={(event) => setPassword(event.target.value)} placeholder="Enter password " />
+        <br /><br />
+        <input type="email" onChange={(event) => setEmail(event.target.value)} placeholder="Enter email " />
+        <br /><br />
 
-<button>Submit</button>
+        <button>Submit</button>
 
-<h3>{name}</h3>
-<h3>{password}
-  <h3>{email}</h3>
-</h3>
-</form>
+        <h3>{name}</h3>
+        <h3>{password}
+          <h3>{email}</h3>
+        </h3>
+      </form>
     </div>
   )
 }
