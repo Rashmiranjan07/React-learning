2d84c56d3e102481c1e4e7692e88b00e2fa79e39
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-23 20:32:39 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -274,35 +274,37 @@
 
 /* Controlled Components */
 
-import { useState } from "react";
-function App() {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+// import { useState } from "react";
+// function App() {
+//   const [name, setName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [email, setEmail] = useState('');
 
-  return (
-    <div>
-      <h1>Controller Component</h1>
-      <form action="" method="get">
+//   return (
+//     <div>
+//       <h1>Controller Component</h1>
+//       <form action="" method="get">
 
-        <input type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter name " />
-        <br /> <br />
-        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter password " />
-        <br /><br />
-        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter email " />
-        <br /><br />
+//         <input type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter name " />
+//         <br /> <br />
+//         <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter password " />
+//         <br /><br />
+//         <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter email " />
+//         <br /><br />
 
-        <button>Submit</button>
-        <button onClick={()=>{setEmail('');setName('');setPassword('')}}>Clear</button>
+//         <button>Submit</button>
+//         <button onClick={()=>{setEmail('');setName('');setPassword('')}}>Clear</button>
 
-        {/* <h3>{name}</h3>
-        <h3>{password}
-          <h3>{email}</h3>
-        </h3> */}
-      </form>
-    </div>
-  )
-}
+//         {/* <h3>{name}</h3>
+//         <h3>{password}
+//           <h3>{email}</h3>
+//         </h3> */}
+//       </form>
+//     </div>
+//   )
+// }
+
+
 
 export default App;
 
