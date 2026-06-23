e56f88565ab6c742eb2cf90cb84457b6c8721b1a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-23 21:07:31 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -306,8 +306,8 @@
 
 /* handle Checkbox */
 import Checkbox from "./Checkbox";
-function App(){
-  return(
+function App() {
+  return (
     <div>
       <h1> Handle Checkbox </h1>
       <Checkbox />
