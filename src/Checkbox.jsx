db2ae314ab5e067a51576db32ba6c862a8bcb3be
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-23 21:06:18 +0530

    added

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -2,22 +2,32 @@ import { useState } from "react";
 
 function Checkbox(){
 
-    const [skills,setSkills]=useState([])
-    
+    const [skills,setSkills]=useState([]);
+    const handleSkills=(event)=>{
+        console.log(event.target.value , event.target.checked);
+        if(event.target.checked){
+            setSkills([...skills,event.target.value])
+        } else {
+            setSkills([...skills.filter((item)=>item!=event.target.value)])
+        }
+    }
+
     return(
         <div>
             <h1>Select your skills</h1>
-            <input type="checkbox" id="php" value="php" />
+            <input onChange={handleSkills} type="checkbox" id="php" value="php" />
             <label htmlFor="php">PHP</label>
             <br /> <br />
-            <input type="checkbox" id="js" value="JS" />
+            <input onChange={handleSkills} type="checkbox" id="js" value="JS" />
             <label htmlFor="js">Js</label>
             <br /> <br />
-            <input type="checkbox" id="node" value="node" />
+            <input onChange={handleSkills} type="checkbox" id="node" value="node" />
             <label htmlFor="node">Node</label>
             <br /> <br />
-            <input type="checkbox" id="java" value="java"/>
+            <input onChange={handleSkills} type="checkbox" id="java" value="java"/>
             <label htmlFor="java">Java</label>
+
+            <h1>{skills.toString()}</h1>
         </div>
     )
 }
