3819a801c527fdd16a8beee57c853e94acea030f
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-24 19:42:53 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -325,15 +325,24 @@ import { useState } from "react";
 function App() {
 
   const [gender, setGender] = useState('female');
+  const [city, setCity] = useState('delhi')
   return (
     <div>
       <h1>Handle Radio and Dropdown</h1>
       <h4>Select gender</h4>
-      <input type="radio" onChange={(event) => setGender(event.target.value)} name="gender" value={"male"} checked={gender=='male'} id="male" />
+      <input type="radio" onChange={(event) => setGender(event.target.value)} name="gender" value={"male"} checked={gender == 'male'} id="male" />
       <label htmlFor="male">Male</label>
-      <input type="radio" onChange={(event) => setGender(event.target.value)} name="gender" value={"female"} checked={gender=='female'} id="female" />
+      <input type="radio" onChange={(event) => setGender(event.target.value)} name="gender" value={"female"} checked={gender == 'female'} id="female" />
       <label htmlFor="female">Female</label>
       <h2>Selected Gender : {gender}</h2>
+      <br /> <br />
+      <h4>Select City</h4>
+      <select onChange={(event) => setCity(event.target.value)} defaultValue={"delhi"}>
+        <option value="noida">Noida</option>
+        <option value="gurgaon">Gurgaon</option>
+        <option value="delhi">Delhi</option>
+      </select>
+      <h2>Selected city : {city}</h2>
     </div>
   )
 }
