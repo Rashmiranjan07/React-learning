c8f09d838c2b9ee87b732085fe3e72d2506fa641
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-06-26 18:42:15 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -899,10 +899,34 @@
 
 /* useRef Hook in React JS */
 
+import { useRef } from "react";
 function App() {
+
+  const inputRef = useRef(null);
+  const inputHandler = () => {
+    console.log(inputRef);
+    inputRef.current.focus();
+    inputRef.current.style.color = 'red';
+    inputRef.current.placeholder = "enter password";
+    inputRef.current.value = "123";
+  }
+
+  const toggleHandler = () => {
+
+    if (inputRef.current.style.display != 'none') {
+      inputRef.current.style.display = 'none'
+    } else {
+      inputRef.current.style.display = 'inline'
+    }
+  }
+
   return (
     <div>
       <h1>useRef Hook</h1>
+      <button onClick={toggleHandler}>Toggle</button>
+
+      <input ref={inputRef} type="text" placeholder="Enter user name" />
+      <button onClick={inputHandler}>Focus on Input field </button>
     </div>
   )
 }
