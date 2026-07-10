a6aeb8b5f5d79232c9471ad75918fb4ec1e9de14
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-10 19:45:13 +0530

    shopper

--- shopper/src/Pages/Shop.jsx
+++ shopper/src/Pages/Shop.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+
+      <Hero />
+
+
+    </div>
   )
 }
 
