4aca4ebbe987f2ade67b4a44dfb2e91beb2e3ea3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-10 21:32:08 +0530

    shopper

--- shopper/src/Pages/Shop.jsx
+++ shopper/src/Pages/Shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
@@ -8,6 +9,7 @@ const Shop = () => {
 
       <Hero />
       <Popular />
+      <Offers />
 
 
     </div>
