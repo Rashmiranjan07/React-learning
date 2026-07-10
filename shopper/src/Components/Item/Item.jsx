e8c64840c73f89200ad5f38e9a37712d26500bf3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-10 20:20:45 +0530

    shopper

---
+++ shopper/src/Components/Item/Item.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Item.css'
+
+const item = (props) => {
+  return (
+    <div className='item'>
+        <img src="" alt="" />
+
+
+    </div>
+  )
+}
+
+export default item
