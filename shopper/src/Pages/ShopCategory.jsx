2cc2ffd9fb1aea6583fa2bc3e81918786ceab705
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-11 20:52:36 +0530

    added

--- shopper/src/Pages/ShopCategory.jsx
+++ shopper/src/Pages/ShopCategory.jsx
@@ -8,7 +8,7 @@ const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>
@@ -35,6 +35,9 @@ const ShopCategory = (props) => {
             return null;
           }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore more
       </div>
     </div>
   )
