1e454a6ce4ee98ce387a2df599c5a86ccdf650dd
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-13 04:49:59 +0530

    shopper

--- shopper/src/Components/Navbar/Navbar.jsx
+++ shopper/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     const [menu, setMenu] = useState("shop");
 
-    const{getTotalCartItems}=useContext(ShopContext);
+    const { getTotalCartItems } = useContext(ShopContext);
 
     return (
         <div className='navbar'>
@@ -18,14 +18,14 @@ const Navbar = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={() => { setMenu("shop") }}> <Link style={{textDecoration:'none'}} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("mens") }}> <Link style={{textDecoration:'none'}} to='/mens'>Men</Link> {menu === "mens" ? <hr /> : <></>} </li>
-                <li onClick={() => { setMenu("womens") }}> <Link style={{textDecoration:'none'}} to='/womens'>Women</Link> {menu === "womens" ? <hr /> : <></>} </li>
-                <li onClick={() => { setMenu("kids") }}> <Link style={{textDecoration:'none'}} to='/kids'>Kid</Link> {menu === "kids" ? <hr /> : <></>} </li>
+                <li onClick={() => { setMenu("shop") }}> <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("mens") }}> <Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link> {menu === "mens" ? <hr /> : <></>} </li>
+                <li onClick={() => { setMenu("womens") }}> <Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link> {menu === "womens" ? <hr /> : <></>} </li>
+                <li onClick={() => { setMenu("kids") }}> <Link style={{ textDecoration: 'none' }} to='/kids'>Kid</Link> {menu === "kids" ? <hr /> : <></>} </li>
             </ul>
-            <div className="nav-login-cart"> 
+            <div className="nav-login-cart">
                 <Link to='/login'><button>Login</button></Link>
-                 <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
+                <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
                 <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
 
