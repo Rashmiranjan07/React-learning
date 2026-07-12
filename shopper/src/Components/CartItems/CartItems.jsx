99f638d14bcc9d710bfa434079d86e1402af6985
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-13 04:11:38 +0530

    shopper

--- shopper/src/Components/CartItems/CartItems.jsx
+++ shopper/src/Components/CartItems/CartItems.jsx
@@ -31,7 +31,30 @@ const CartItems = () => {
                         <hr />
                     </div>
                 }
+                return null;
             })}
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>Cart Total</h1>
+                    <div>
+                       <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>${0}</p>
+                        </div> 
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping Fee</p>
+                            <p>Free</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <h3>Total</h3>
+                            <h3>${0}</h3>
+                        </div>
+                    </div>
+                    <button>Proceed to Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
